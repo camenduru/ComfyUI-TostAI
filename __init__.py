71498fca56bb9ebef6d7f43f3ede1d615cf50983
camenduru <54370274+camenduru@users.noreply.github.com>
2024-08-22 02:30:12 +0300

    test

--- __init__.py
+++ __init__.py
@@ -24,7 +24,7 @@ class SendToTostAIWebhook:
 
     def send_to_tost_ai_webhook(self, images, webhook_url: str, webhook_key: str,):
         (full_output_folder, filename, counter, subfolder, _,) = folder_paths.get_save_image_path("final_tost", folder_paths.get_temp_directory())
-        tost_file_path = os.path.join(full_output_folder, f"{filename}_.png")
+        tost_file_path = os.path.join(full_output_folder, f"{filename}.png")
         tost_image = 255.0 * images[0].cpu().numpy()
         tost_image_pil = Image.fromarray(tost_image.astype(np.uint8))
         tost_image_pil.save(tost_file_path)
